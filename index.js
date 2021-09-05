334b3fd9603f24ada7d369588a4e05dac9d9e42e
drox13 <dario.baron@uptc.edu.co>
2021-09-05 12:27:40 -0500

    se transfirieron archivos

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-var express = require('express');
-var app = express();
-const port = 9000;
+const express = require('express')
+const axios = require('axios')
+const Jimp = require('jimp')
+const app = express()
+const port = 4000
 
-app.get('/', function(req, res){
-   res.send("Esta es una instancia Corriendo!");
-});
+app.use(express.json({limit: '10mb'}))
 
 app.listen(port, () => {
-   console.log(`Example app listening at http://localhost:${port}`);
- });
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+app.post('/image', (req, res) => {
+  console.log(req.body)
+  Jimp.read(Buffer.from(req.body.buffer.data))
+    .then(image => {
+      const height = image.bitmap.height;
+      const width = image.bitmap.width;
+      axios.get('https://geek-jokes.sameerkumar.website/api?format=json').then(function (response) {
+            Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(font => {
+              image.print(font, width * 20 / 100, height * 80 / 100, response.data.joke);
+              res.send({width: width, height: height, mimetype: req.body.mimetype, buffer: Buffer.from(image.bitmap.data)})
+            });
+        }).catch(function (error) {
+                console.error('Error ' + error.message)
+            })
+    }).catch(err => {
+    console.log(err)
+  });
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+
