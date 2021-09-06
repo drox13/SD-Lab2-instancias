7e5c5299804d041485213b260c765a8fcc59a55f
drox13 <dario.baron@uptc.edu.co>
2021-09-06 18:15:21 -0500

    Se modificaron las instancias para el precesamiento de la imagen

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 const express = require('express')
 const axios = require('axios')
 const Jimp = require('jimp')
+var ip = require("ip");
 const app = express()
 const port = 4000
 
-app.use(express.json({limit: '10mb'}))
+app.use(express.json({ limit: '10mb' }))
+app.use("/public", express.static("/public"));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 app.post('/image', (req, res) => {
-  console.log(req.body)
   Jimp.read(Buffer.from(req.body.buffer.data))
     .then(image => {
       const height = image.bitmap.height;
       const width = image.bitmap.width;
       axios.get('https://geek-jokes.sameerkumar.website/api?format=json').then(function (response) {
-            Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(font => {
-              image.print(font, width * 20 / 100, height * 80 / 100, response.data.joke);
-              res.send({width: width, height: height, mimetype: req.body.mimetype, buffer: Buffer.from(image.bitmap.data)})
-            });
-        }).catch(function (error) {
-                console.error('Error ' + error.message)
-            })
+        Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(font => {
+          image.print(font, width * 20 / 100, height * 80 / 100, response.data.joke);
+          image.resize(800, 600);
+          res.send({width: image.bitmap.width, height: image.bitmap.height, pixels: Buffer.from(image.bitmap.data)})
+        });
+      }).catch(function (error) {
+        console.error('Error ' + error.message)
+      })
     }).catch(err => {
-    console.log(err)
-  });
+      console.log(err)
+    });
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
+  res.send('Hello World!')
+})
+
 
